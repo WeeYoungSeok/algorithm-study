f9cc306e55910e76d922cf7748774871c31b142b
weeyoungseok <dndrldzk@gmail.com>
2026-02-27 17:05:07 +0900

    docs: 풀이 방법 주석 추가

--- BOJ/greedy/BOJ_17615.py
+++ BOJ/greedy/BOJ_17615.py
@@ -1,13 +1,16 @@
 import sys
 
 # [문제 링크]
-# 
+# https://www.acmicpc.net/problem/17615
 
 # [문제 정보]
-# 분류 : 
-# 난이도 : 
+# 분류 : BOJ 17615 : 볼 모으기
+# 난이도 : 실버 1
 
 # [풀이 방법]
+# 공을 옮기는 과정이 아닌 최종 결과 상태에 집중함.
+# 목표 방향 끝에 이미 붙어 있는 같은 색 공들은 이동 대상에서 제외함.
+# lstrip/rstrip을 사용하여 정렬된 공을 지우고 남은 공의 개수를 세어 최소값을 반환함.
 
 input = sys.stdin.readline
 
@@ -71,44 +74,8 @@ def clean_solution():
     print(min_count)
     return
 
-def ultra_clean_solution():
-    n = int(input())
-    balls = input().strip()
-
-    # 1. 전체 빨간 공과 파란 공의 개수를 미리 센다 (O(N))
-    total_red = balls.count('R')
-    total_blue = n - total_red # 전체에서 빨간 거 빼면 파란 거
-
-    # 2. 왼쪽 끝에서 연속된 공의 개수 세기 (O(N)이지만 사실상 아주 짧음)
-    left_count = 0
-    for i in range(n):
-        if balls[i] == balls[0]:
-            left_count += 1
-        else:
-            break
-
-    # 3. 오른쪽 끝에서 연속된 공의 개수 세기
-    right_count = 0
-    for i in range(n-1, -1, -1):
-        if balls[i] == balls[-1]:
-            right_count += 1
-        else:
-            break
-
-    # 4. 4가지 케이스 계산
-    # 빨간 공을 왼쪽으로: 전체 빨간 공 - (만약 왼쪽 끝이 빨간색이면 그 개수만큼 이득)
-    res1 = total_red - (left_count if balls[0] == 'R' else 0)
-    # 빨간 공을 오른쪽으로: 전체 빨간 공 - (만약 오른쪽 끝이 빨간색이면 그 개수만큼 이득)
-    res2 = total_red - (right_count if balls[-1] == 'R' else 0)
-    # 파란 공을 왼쪽으로
-    res3 = total_blue - (left_count if balls[0] == 'B' else 0)
-    # 파란 공을 오른쪽으로
-    res4 = total_blue - (right_count if balls[-1] == 'B' else 0)
-
-    print(min(res1, res2, res3, res4))
 
 
 if __name__ == "__main__":
     # solution()
     clean_solution()
-    ultra_clean_solution()
